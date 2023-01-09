b65746c36177d104593435a7eaebfa5a5b118cf6
singularityRx0 <94662590+singularityRx0@users.noreply.github.com>
2023-01-09 16:30:31 +0800

    Create sp_validateLogin.sql

---
+++ arduino_attendence/sp_validateLogin.sql
@@ -0,0 +1,8 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validateLogin`(
+IN p_username VARCHAR(45)
+)
+BEGIN
+    select * from tbl_user where user_username = p_username;
+END$$
+DELIMITER ;
